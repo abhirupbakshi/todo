35d224b113d6dd3d4aace88a7128ee45d19dd132
Abhirup Bakshi <119347931+abhirupbakshi@users.noreply.github.com>
2023-08-25 17:31:24 +0530

    Update TodoServer.java

--- src/main/java/com/example/todo/TodoServer.java
+++ src/main/java/com/example/todo/TodoServer.java
@@ -1,10 +1,7 @@
 package com.example.todo;
 
-import com.example.todo.model.Role;
-import com.example.todo.repository.RoleRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TodoServer {
